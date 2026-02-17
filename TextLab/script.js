362d4841b7fafdc677159990ff9433d58fdd0c60
Charles Nextime <iamdxn@gmail.com>
2026-02-17 16:24:55 +0800

    Update script.js

--- TextLab/script.js
+++ TextLab/script.js
@@ -1,3 +1,12 @@
+/**
+ * Charles Nextime Web Tools Portal - Core Logic
+ * * Copyright (c) 2026 Charles Nextime
+ * Licensed under the GNU General Public License v3.0
+ * * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ */
+
 let history = [];
 const in1 = document.getElementById('inputArea');
 const in2 = document.getElementById('inputArea2');
