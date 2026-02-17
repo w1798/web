2b8d20aec1b3b0d5846ef20cf6e039f6a2f2d943
Charles Nextime <iamdxn@gmail.com>
2026-02-17 16:23:02 +0800

    Update utils.js

--- evalprompt/utils.js
+++ evalprompt/utils.js
@@ -1,3 +1,12 @@
+/**
+ * Charles Nextime Web Tools Portal - Core Logic
+ * * Copyright (c) 2026 Charles Nextime
+ * Licensed under the GNU General Public License v3.0
+ * * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ */
+
 // 全域狀態變數
 let config, studentStates, activeStudent, secondaryColors;
 
