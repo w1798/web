2f070be93964ea6f99de442176a78dcef5324fe1
Charles Nextime <iamdxn@gmail.com>
2026-02-17 16:22:30 +0800

    Update main.js

--- evalprompt/main.js
+++ evalprompt/main.js
@@ -1,3 +1,12 @@
+/**
+ * Charles Nextime Web Tools Portal - Core Logic
+ * * Copyright (c) 2026 Charles Nextime
+ * Licensed under the GNU General Public License v3.0
+ * * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ */
+
 // 全域狀態初始化
 config = JSON.parse(localStorage.getItem('eval_v6_config')) || { ...defaultConfig };
 studentStates = JSON.parse(localStorage.getItem('eval_v6_states')) || {}; 
